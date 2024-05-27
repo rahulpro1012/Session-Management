e15258739c95c49521a69af828eb2a8626e609ac
rahulpro1012 <rahulmudaliar1012@gmail.com>
2024-05-27 10:48:26 +0530

    Initial commit

---
+++ tut1/src/test/java/com/rahul/tut1/Tut1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.tut1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tut1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
